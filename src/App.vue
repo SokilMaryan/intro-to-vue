93f1f81b2f2f05242bf3bdc0e05286219589a5bd
Marian S <88345718+SokilMaryan@users.noreply.github.com>
2022-11-28 13:58:39 +0200

    1

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="max-w-[1440px] p-6">
+    <h3 class="font-medium m-0">Contact list</h3>
+
+    <button
+      class="text-gray border-solid rounded-xl border p-2 mt-2 hover:bg-white"
+      @click="clickAddContact = true"
+    >
+      Add contact
+    </button>
+
+    <div class="contact-list grid-cols-[repeat(auto-fill,_minmax(320px,_1fr))] grid gap-5 my-5">
+      <ContactItem
+        v-if="clickAddContact"
+        :contact="{description: '', name: '', image: '', id: 0}"
+        @save="createContact"
+        @delete="deleteContact"
+        @cancel="clickOnCancel"
+      />
+
+      <ContactItem
+        v-for="(contact, index) in contacts"
+        :key="contact.id"
+        :contact="contact"
+        @delete="deleteContact(index)"
+        @save="onContactSave($event, index)"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { IContact } from '@/types'
+import ContactItem from '@/components/ContactItem.vue'
+
+const contacts = ref<IContact[]>([
+  {
+    id: 1,
+    name: 'Esther Howard',
+    description: 'Forward Response Developer',
+    image: 'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+  },
+  {
+    id: 2,
+    name: 'Jane Cooper',
+    description: 'Regional Paradigm Technician Regional Paradigm Technician Regional Paradigm Technician',
+    image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+  },
+  {
+    id: 3,
+    name: 'Cody Fisher',
+    description: 'Product Directives Officer',
+    image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+  }
+])
+
+const clickAddContact = ref(false)
+
+function deleteContact (index: number) {
+  contacts.value.splice(index, 1)
+}
+
+function onContactSave (contact: IContact, index: number) {
+  contacts.value[index] = { ...contact }
+}
+
+function clickOnCancel () {
+  clickAddContact.value = false
+}
+
+function createContact (contact: IContact) {
+  clickAddContact.value = false
+  contact.id = Math.floor(Math.random() * 100000000000)
+  console.log(contact, contact.id)
+  contacts.value.unshift({ ...contact })
+}
+
+</script>
